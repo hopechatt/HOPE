fa061f584d602d2b5e3d14456f3ae0bd8aac856b
hopechatt <hoperideshare@zohomail.com>
2026-06-09 17:37:42 -0400

    Set ADMIN_UID for Hope Latrice Schiesser

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const SUPABASE_URL = "https://aqvzaunbbdqfaqmojute.supabase.co";
 export const SUPABASE_ANON_KEY = "sb_publishable_WtH6WgPGqqtaUfungk7VuQ_V0AUce_2";
 export const GOOGLE_MAPS_API_KEY = "YOUR_GOOGLE_MAPS_API_KEY";
-export const ADMIN_UID = "YOUR_SUPABASE_AUTH_USER_ID";
+export const ADMIN_UID = "1863af19-d0d3-4655-9363-7a810aecaa93";
